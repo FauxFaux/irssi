623fdf1b8732d972564e64bb68d8e367267154d7
Chris West (Faux) <git@goeswhere.com>
2013-12-27 17:46:52 +0000

    can publish to zmq, no security, global bind

--- src/irc/irssicp/irssicp.c
+++ src/irc/irssicp/irssicp.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#include <zmq.h>
+
 #define MODULE_NAME "irssicp"
 
 #include "irc.h"
@@ -19,55 +21,29 @@
 #include "fe-text/gui-windows.h"
 #include "fe-common/core/printtext.h"
 
-static GSList *irssicp_listens;
+static void *ctx;
+static void *pubsock;
 
 static GString *next_line;
 static int ignore_next;
 
-
-typedef struct {
-	int port;
-	char *ircnet;
-
-	int tag;
-	GIOChannel *handle;
-
-	GSList *clients;
-} LISTEN_REC;
-
-typedef struct {
-	char *nick, *host;
-	NET_SENDBUF_REC *handle;
-	int recv_tag;
-	char *irssicp_address;
-	LISTEN_REC *listen;
-	IRC_SERVER_REC *server;
-	unsigned int pass_sent:1;
-	unsigned int user_sent:1;
-	unsigned int connected:1;
-	unsigned int want_ctcp:1;
-} CLIENT_REC;
-
-static CLIENT_REC connected_client = {};
-
-static void irssicp_outdata(CLIENT_REC *client, const char *data, ...)
+static void irssicp_outdata(void *sock, const char *data, ...)
 {
-	va_list args;
-	char *str;
+    va_list args;
+    char *str;
 
-	g_return_if_fail(client != NULL);
-	g_return_if_fail(data != NULL);
+    g_return_if_fail(data != NULL);
 
-	va_start(args, data);
+    va_start(args, data);
 
-	str = g_strdup_vprintf(data, args);
-	net_sendbuffer_send(client->handle, str, strlen(str));
-	g_free(str);
+    str = g_strdup_vprintf(data, args);
+    zmq_send(sock, str, strlen(str), 0);
+    g_free(str);
 
-	va_end(args);
+    va_end(args);
 }
 
-static void irssicp_dump_data(CLIENT_REC *client) {
+static void irssicp_dump_data(void *sock) {
     GString *text = g_string_new(NULL);
     GSList *win_item;
     for (win_item = windows; win_item != NULL; win_item = win_item->next) {
@@ -76,30 +52,12 @@ static void irssicp_dump_data(CLIENT_REC *client) {
         LINE_REC *line = buf->first_line;
         for (; line != NULL; line = line->next) {
             textbuffer_line2text(line, TRUE, text);
-            irssicp_outdata(client, "%d %d %s\n", line->info.time, win_rec->refnum, text->str);
+            irssicp_outdata(sock, "%d %d %s\n", line->info.time, win_rec->refnum, text->str);
         }
     }
 }
 
-static void remove_client(CLIENT_REC *rec)
-{
-	g_return_if_fail(rec != NULL);
-
-	irssicp_clients = g_slist_remove(irssicp_clients, rec);
-	rec->listen->clients = g_slist_remove(rec->listen->clients, rec);
-
-	signal_emit("irssicp client disconnected", 1, rec);
-	printtext(NULL, NULL, MSGLEVEL_CLIENTNOTICE,
-		  "Proxy: Client disconnected from %s", rec->host);
-
-	g_free(rec->irssicp_address);
-	net_sendbuffer_destroy(rec->handle, TRUE);
-	g_source_remove(rec->recv_tag);
-	g_free_not_null(rec->nick);
-	g_free_not_null(rec->host);
-	g_free(rec);
-}
-
+#if 0
 static void irssicp_redirect_event(CLIENT_REC *client, const char *command,
 				 int count, const char *arg, int remote)
 {
@@ -111,32 +69,6 @@ static void irssicp_redirect_event(CLIENT_REC *client, const char *command,
 	server_redirect_event(client->server, command, count,
 			      arg, remote, NULL, "", str, NULL);
 	g_free(str);
-}
-
-static void handle_client_connect_cmd(CLIENT_REC *client,
-				      const char *cmd, const char *args)
-{
-	const char *password;
-
-	password = settings_get_str("irssiirssicp_password");
-
-	if (password != NULL && strcmp(cmd, "PASS") == 0) {
-		if (strcmp(password, args) == 0)
-			client->pass_sent = TRUE;
-		else {
-			/* wrong password! */
-			remove_client(client);
-            return;
-		}
-    }
-
-    if (*password != '\0' && !client->pass_sent) {
-        /* client didn't send us PASS, kill it */
-        remove_client(client);
-    } else {
-        client->connected = TRUE;
-        irssicp_dump_data(client);
-    }
 }
 
 static void handle_client_cmd(CLIENT_REC *client, char *cmd, char *args,
@@ -211,159 +143,17 @@ static void sig_listen_client(CLIENT_REC *client)
 	}
 }
 
-static void sig_listen(LISTEN_REC *listen)
-{
-	CLIENT_REC *rec;
-	IPADDR ip;
-	NET_SENDBUF_REC *sendbuf;
-        GIOChannel *handle;
-	char host[MAX_IP_LEN];
-	int port;
-
-	g_return_if_fail(listen != NULL);
-
-	/* accept connection */
-	handle = net_accept(listen->handle, &ip, &port);
-	if (handle == NULL)
-		return;
-	net_ip2host(&ip, host);
-	sendbuf = net_sendbuffer_create(handle, 0);
-	rec = g_new0(CLIENT_REC, 1);
-	rec->listen = listen;
-	rec->handle = sendbuf;
-    rec->host = g_strdup(host);
-	if (strcmp(listen->ircnet, "*") == 0) {
-		rec->irssicp_address = g_strdup("irc.irssicp");
-		rec->server = servers == NULL ? NULL : IRC_SERVER(servers->data);
-	} else {
-		rec->irssicp_address = g_strdup_printf("%s.irssicp", listen->ircnet);
-		rec->server = servers == NULL ? NULL :
-			IRC_SERVER(server_find_chatnet(listen->ircnet));
-	}
-	rec->recv_tag = g_input_add(handle, G_INPUT_READ,
-			       (GInputFunction) sig_listen_client, rec);
-
-	irssicp_clients = g_slist_prepend(irssicp_clients, rec);
-	rec->listen->clients = g_slist_prepend(rec->listen->clients, rec);
-
-        signal_emit("irssicp client connected", 1, rec);
-	printtext(NULL, NULL, MSGLEVEL_CLIENTNOTICE,
-		  "Proxy: Client connected from %s", rec->host);
-}
-
-static LISTEN_REC *find_listen(const char *ircnet, int port)
-{
-	GSList *tmp;
-
-	for (tmp = irssicp_listens; tmp != NULL; tmp = tmp->next) {
-		LISTEN_REC *rec = tmp->data;
-
-		if (rec->port == port &&
-		    g_strcasecmp(rec->ircnet, ircnet) == 0)
-			return rec;
-	}
-
-	return NULL;
-}
-
-static void add_listen(const char *ircnet, int port)
-{
-	LISTEN_REC *rec;
-	IPADDR ip4, ip6, *my_ip;
-
-	if (port <= 0 || *ircnet == '\0')
-		return;
-
-	/* bind to specific host/ip? */
-	my_ip = NULL;
-	if (*settings_get_str("irssiirssicp_bind") != '\0') {
-		if (net_gethostbyname(settings_get_str("irssiirssicp_bind"),
-				      &ip4, &ip6) != 0) {
-			printtext(NULL, NULL, MSGLEVEL_CLIENTERROR,
-				  "Proxy: can not resolve '%s' - aborting",
-				  settings_get_str("irssiirssicp_bind"));
-			return;
-		}
-
-		my_ip = ip6.family == 0 ? &ip4 : ip4.family == 0 ||
-			settings_get_bool("resolve_prefer_ipv6") ? &ip6 : &ip4;
-	}
-
-	rec = g_new0(LISTEN_REC, 1);
-	rec->ircnet = g_strdup(ircnet);
-	rec->port = port;
-
-	rec->handle = net_listen(my_ip, &rec->port);
-
-	if (rec->handle == NULL) {
-		printtext(NULL, NULL, MSGLEVEL_CLIENTERROR,
-			  "Proxy: Listen in port %d failed: %s",
-			  rec->port, g_strerror(errno));
-		g_free(rec->ircnet);
-                g_free(rec);
-		return;
-	}
-
-	rec->tag = g_input_add(rec->handle, G_INPUT_READ,
-			       (GInputFunction) sig_listen, rec);
-
-        irssicp_listens = g_slist_append(irssicp_listens, rec);
-}
-
-static void remove_listen(LISTEN_REC *rec)
-{
-	irssicp_listens = g_slist_remove(irssicp_listens, rec);
-
-	while (rec->clients != NULL)
-		remove_client(rec->clients->data);
-
-	net_disconnect(rec->handle);
-	g_source_remove(rec->tag);
-	g_free(rec->ircnet);
-	g_free(rec);
-}
-
-static void read_settings(void)
-{
-	LISTEN_REC *rec;
-	GSList *remove_listens;
-	char **ports, **tmp, *ircnet, *port;
-	int portnum;
-
-	remove_listens = g_slist_copy(irssicp_listens);
-
-	ports = g_strsplit(settings_get_str("irssiirssicp_ports"), " ", -1);
-	for (tmp = ports; *tmp != NULL; tmp++) {
-		ircnet = *tmp;
-		port = strchr(ircnet, '=');
-		if (port == NULL)
-			continue;
-
-		*port++ = '\0';
-		portnum = atoi(port);
-		if (portnum <=  0)
-			continue;
-
-		rec = find_listen(ircnet, portnum);
-		if (rec == NULL)
-			add_listen(ircnet, portnum);
-		else
-			remove_listens = g_slist_remove(remove_listens, rec);
-	}
-	g_strfreev(ports);
-
-	while (remove_listens != NULL) {
-                remove_listen(remove_listens->data);
-		remove_listens = g_slist_remove(remove_listens, remove_listens->data);
-	}
-}
-
+#endif
 static void sig_print_text(WINDOW_REC *win) {
-    GSList *tmp;
-//        irssicp_outdata(current_, "%d %s\n", win->refnum, text);
+    LINE_REC *l = textbuffer_line_last(WINDOW_GUI(win
+                )->view->buffer);
+    GString *str = g_string_new(NULL);
+    textbuffer_line2text(l, 0, str);
+    irssicp_outdata(pubsock, "%d %s", win->refnum, str->str);
+    g_string_free(str, 1);
 }
 
-static void sig_dump(CLIENT_REC *client, const char *data)
+static void sig_dump(void *client, const char *data)
 {
 	g_return_if_fail(client != NULL);
 	g_return_if_fail(data != NULL);
@@ -371,58 +161,57 @@ static void sig_dump(CLIENT_REC *client, const char *data)
 	irssicp_outdata(client, data);
 }
 
-static void irssicp_listen_init(void)
+gboolean zmq_gio_worker(GIOChannel *source, GIOCondition condition, gpointer data)
 {
-	next_line = g_string_new(NULL);
+    uint32_t status;
+    size_t sizeof_status = sizeof(status);
 
-	memset(&connected_client, 0, sizeof(connected_client));
-	irssicp_listens = NULL;
-	read_settings();
+    if (zmq_getsockopt(data, ZMQ_EVENTS, &status, &sizeof_status)) {
+        signal_emit("gui dialog", 2, "warning", "retrieving event status failed; doom");
+        return 0;
+    }
 
-	signal_add("setup changed", (SIGNAL_FUNC) read_settings);
-	
-    signal_add("gui print text finished", (SIGNAL_FUNC) sig_print_text);
-	signal_add("irssicp client dump", (SIGNAL_FUNC) sig_dump);
+    signal_emit("gui dialog", 2, "warning", "twerk");
+
+    if ((status & ZMQ_POLLIN) == 0) {
+        return 1;
+    }
+
+    // do work
+
+    return 1; // keep the callback active
 }
 
-static void irssicp_listen_deinit(void)
+static void irssicp_listen_init(void)
 {
-	while (irssicp_listens != NULL)
-		remove_listen(irssicp_listens->data);
-	g_string_free(next_line, TRUE);
-
-	signal_remove("setup changed", (SIGNAL_FUNC) read_settings);
-
-    signal_remove("gui print text finished", (SIGNAL_FUNC) sig_print_text);
-	signal_remove("irssicp client dump", (SIGNAL_FUNC) sig_dump);
+    next_line = g_string_new(NULL);
+    pubsock = zmq_socket(ctx, ZMQ_PUB);
+    if (zmq_bind(pubsock, "tcp://*:5556")) {
+        signal_emit("gui dialog", 2, "warning", "unbindable");
+    }
+#if 0
+    int fd;
+    size_t sizeof_fd = sizeof(fd);
+    if (zmq_getsockopt(pubsock, ZMQ_FD, &fd, &sizeof_fd)) {
+        signal_emit("gui dialog", 2, "warning", "fd extraction failed; we're screwed");
+    }
+    GIOChannel *ichan = g_io_channel_unix_new(fd);
+    g_io_add_watch(ichan, G_IO_IN|G_IO_ERR|G_IO_HUP, zmq_gio_worker, pubsock);
+#endif
+    signal_add("gui print text finished", (SIGNAL_FUNC) sig_print_text);
+    signal_add("irssicp client dump", (SIGNAL_FUNC) sig_dump);
 }
 
 void irc_irssicp_init(void)
 {
-	settings_add_str("irssiirssicp", "irssiirssicp_ports", "");
-	settings_add_str("irssiirssicp", "irssiirssicp_password", "");
-	settings_add_str("irssiirssicp", "irssiirssicp_bind", "");
-
-	if (*settings_get_str("irssiirssicp_password") == '\0') {
-		/* no password - bad idea! */
-		signal_emit("gui dialog", 2, "warning",
-			    "Warning!! Password not specified, everyone can "
-			    "use this irssicp! Use /set irssiirssicp_password "
-			    "<password> to set it");
-	}
-	if (*settings_get_str("irssiirssicp_ports") == '\0') {
-		signal_emit("gui dialog", 2, "warning",
-			    "No irssicp ports specified. Use /SET "
-			    "irssiirssicp_ports <ircnet>=<port> <ircnet2>=<port2> "
-			    "... to set them.");
-	}
-
-	irssicp_listen_init();
-	settings_check();
-        module_register("irssicp", "irc");
+    ctx = zmq_ctx_new();
+    settings_add_str("irssiirssicp", "irssiirssicp_bind", "");
+    irssicp_listen_init();
+    settings_check();
+    module_register("irssicp", "irc");
 }
 
 void irc_irssicp_deinit(void)
 {
-	irssicp_listen_deinit();
+    zmq_term(ctx);
 }
